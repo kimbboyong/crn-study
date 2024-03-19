d53523b3bb55175b5d54a221fd079a9ca551286d
kimbboyong <kimbboyong@gmail.com>
2024-03-19 15:08:52 +0900

    react-rsp-component-change

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,27 +1,29 @@
+import { Component } from 'react';
 import { GridTitle, Content, Result, Tie, Win, Lose } from '../../style/StyledBox';
 
-const Box = ({ title, item, result }) => {
-    let ResultComponent = Tie;
-    if (result === 'win') {
-        ResultComponent = Win;
-    } else if (result === 'lose') {
-        ResultComponent = Lose;
+export default class Box extends Component {
+    render() {
+        let ResultComponent = Tie;
+        if (this.props.result === 'win') {
+            ResultComponent = Win;
+        } else if (this.props.result === 'lose') {
+            ResultComponent = Lose;
+        }
+        return (
+            <ResultComponent>
+                <GridTitle>
+                    {this.props.title}
+                </GridTitle>
+                <Content>
+                    <figure>
+                        <img src={this.props.item && this.props.item.img} alt="" />
+                    </figure>
+                    <Result>
+                        {this.props.result}
+                    </Result>
+                </Content>
+            </ResultComponent>
+        );
     }
-    return (
-        <ResultComponent>
-            <GridTitle>
-                {title}
-            </GridTitle>
-            <Content>
-                <figure>
-                    <img src={item && item.img} alt="" />
-                </figure>
-                <Result>
-                    {result}
-                </Result>
-            </Content>
-        </ResultComponent>
-    );
-}
 
-export default Box;
+}
